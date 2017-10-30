cf546542a70a04b1db0a23f84f6df0194712d072
BeijiYang <langyuyang@outlook.com>
2017-10-30 20:03:28 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
 const fs = require('fs')
 let intermediaArr = []
 
-try {
-  let data = fs.readFileSync('data.js', 'utf-8')
-    .split(/\r?\n/)
-    .map(
-      item => {
-        let jsonifyItem = JSON.parse(item)
-        let tempObj = {}
-        tempObj._id = jsonifyItem._id
-        tempObj.username = jsonifyItem.username
-        tempObj.password = jsonifyItem.services.password.bcrypt
-        tempObj.paidCourses = jsonifyItem.paidCourses
-        tempObj.createdAt = jsonifyItem.createdAt.$date
-        intermediaArr.push(tempObj)
+// 用户数据
+let userData = fs.readFileSync('hqusers.json', 'utf-8')
+  .split(/\r?\n/)
+userData.pop()
+
+userData.forEach(
+    item => {
+      let jsonifyItem = JSON.parse(item)
+      let tempObj = {}
+      tempObj._id = jsonifyItem._id
+      tempObj.username = jsonifyItem.username
+      tempObj.password = jsonifyItem.services.password.bcrypt
+      tempObj.admin = jsonifyItem.admin ? jsonifyItem.admin : false
+      tempObj.wechatId = null
+      tempObj.mails = jsonifyItem.emails
+      tempObj.contracts = []
+      tempObj.createdAt = jsonifyItem.createdAt.$date
+      intermediaArr.push(tempObj)
+    }
+  )
+
+// Users Colletcion without contracts
+console.log(intermediaArr.length);
+
+
+// VIP数据
+let vipData = fs.readFileSync('vipdata.json', 'utf-8')
+  .split(/\r?\n/)
+vipData.pop()
+
+// 课程数据
+let courseData = fs.readFileSync('coursedata.json', 'utf-8')
+  .split(/\r?\n/)
+courseData.pop()
+// console.log(JSON.parse(courseData[0]).type)
+
+let vipNum = 0
+let courseNum = 0
+intermediaArr.forEach(
+  user => {
+    vipData.forEach(
+      vip => {
+        if (user._id === JSON.parse(vip).userId) {
+          user.contracts.push(JSON.parse(vip)._id)
+          ++vipNum
+        }
       }
     )
-} catch (err) {
-    // console.log(err);
-}
+
+    courseData.forEach(
+      course => {
+        if (user._id === JSON.parse(course).userId) {
+          user.contracts.push(JSON.parse(course)._id)
+          ++courseNum
+        }
+      }
+    )
+  }
+)
+
+console.log(vipNum)
+console.log(courseNum)
 
 let finalStr = intermediaArr.map(
   item => JSON.stringify(item)
 ).join('\n')
 
-fs.writeFile('temp.js', finalStr, (err) => {
+fs.writeFile('user.js', finalStr, (err) => {
   if (err) throw err
   console.log('The file has been restructured!')
 })
+// mongoimport -d test -c testcollection --file temp.js
